7675617765b719ddcc7d2cdd42939930f6534709
Rijk Vogels <rwvogels@gmail.com>
2024-06-08 16:26:31 +0200

    Added Empty Cell Display

--- Sudoku/FrontEnd/Modules/Factories/DefiniteView.cs
+++ Sudoku/FrontEnd/Modules/Factories/DefiniteView.cs
@@ -17,12 +17,12 @@ namespace FrontEnd.Modules
                     ICell? cell = sudoku.Grid[row, col];
                     if (cell is null)
                         yield return new ColoredString("   ", ConsoleColor.White, ConsoleColor.Black); // Display an empty cell for Samurai puzzles.
+
                     else
                     {
                         string topBorder = " ";
                         if (row > 0 && sudoku.Grid[row - 1, col] is not null && cell.Field != sudoku.Grid[row - 1, col].Field)
                             topBorder = "—";
-
                         yield return new ColoredString(" " + topBorder + " ", Display.BorderColor, Display.BackgroundColor);
                     }
                 }
@@ -40,14 +40,17 @@ namespace FrontEnd.Modules
                         string leftBorder = " ";
                         if (col > 0 && sudoku.Grid[row, col - 1] is not null && cell.Field != sudoku.Grid[row, col - 1].Field)
                             leftBorder = "|";
+                        yield return new ColoredString(leftBorder, Display.BorderColor, Display.BackgroundColor);
+
+                        string cellValue = cell.Value == 0 ? " " : cell.Value.ToString();
+                        yield return new ColoredString(cellValue, Display.ValueColor, Display.BackgroundColor);
 
                         string rightBorder = " ";
                         if (col < size - 1 && sudoku.Grid[row, col + 1] is not null && cell.Field != sudoku.Grid[row, col + 1].Field)
                             rightBorder = "|";
-
-                        yield return new ColoredString(leftBorder, Display.BorderColor, Display.BackgroundColor);
-                        yield return new ColoredString(cell.Value.ToString(), Display.ValueColor, Display.BackgroundColor);
                         yield return new ColoredString(rightBorder, Display.BorderColor, Display.BackgroundColor);
+                        
+                        
                     }
                 }
                 yield return new ColoredString(Environment.NewLine, ConsoleColor.White, ConsoleColor.Black);
@@ -64,7 +67,6 @@ namespace FrontEnd.Modules
                         string bottomBorder = " ";
                         if (row < size - 1 && sudoku.Grid[row + 1, col] is not null && cell is not null && cell.Field != sudoku.Grid[row + 1, col].Field)
                             bottomBorder = "—";
-
                         yield return new ColoredString(" " + bottomBorder + " ", Display.BorderColor, Display.BackgroundColor);
                     }
                 }
