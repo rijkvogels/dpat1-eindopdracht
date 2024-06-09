46a66670c1038b67b38f242c5cdbc957d0224adc
Rijk Vogels <rwvogels@gmail.com>
2024-06-09 23:22:34 +0200

    Added Parameters to Validator

--- Sudoku/GameLibrary/Cell.cs
+++ Sudoku/GameLibrary/Cell.cs
@@ -13,12 +13,10 @@
             this.Auxiliaries = [];
         }
 
-        public bool Validate()
+        public bool Validate(ISudoku sudoku, int HorizontalPosition, int VerticalPosition)
         {
             // TODO: Create a Validator for the cell.
-            // TODO: This validator should have the paramteres, sudoku, positionHorizontal and positionVertical.
 
-            // TODO: I THINK WE SHOULD MOVE VALIDATE FROM VIEWS TO GAME AFTER A NEW VALUE GETS UPDATED TO LIMIT THE AMOUNT OF TIMES IT GETS CALLED AND FOR MVC.
             return false;
         }
     }
@@ -29,6 +27,6 @@
         int Field { get; }
         int[] Auxiliaries { get; set; }
 
-        bool Validate();
+        bool Validate(ISudoku sudoku, int HorizontalPosition, int VerticalPosition);
     }
 }
